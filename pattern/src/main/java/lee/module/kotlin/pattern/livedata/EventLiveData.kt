5636afd7a89ede57efbe1fbcb364f0d0148cc92c
Long Do <longdt12@onemount.com>
2022-06-02 15:48:24 +0700

    Add Kotlin Pattern

---
+++ pattern/src/main/java/lee/module/kotlin/pattern/livedata/EventLiveData.kt
@@ -0,0 +1,78 @@
+package lee.module.kotlin.pattern.livedata
+
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.Observer
+import java.util.Collections
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicBoolean
+
+/**
+ * Class representing live data that emits one-shot events
+ * Guarantees that once consumed event is not redelivered
+ */
+class EventLiveData<T> : MediatorLiveData<T>() {
+
+    private val observers = ConcurrentHashMap<LifecycleOwner, MutableSet<ObserverWrapper<T>>>()
+
+    @MainThread
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        val wrapper = ObserverWrapper(observer)
+        val set = observers[owner]
+        set?.apply {
+            add(wrapper)
+        } ?: run {
+            val newSet = Collections.newSetFromMap(ConcurrentHashMap<ObserverWrapper<T>, Boolean>())
+            newSet.add(wrapper)
+            observers[owner] = newSet
+        }
+        super.observe(owner, wrapper)
+    }
+
+    override fun removeObservers(owner: LifecycleOwner) {
+        observers.remove(owner)
+        super.removeObservers(owner)
+    }
+
+    override fun removeObserver(observer: Observer<in T>) {
+        observers.forEach {
+            if (it.value.remove(observer as Observer<*>)) {
+                if (it.value.isEmpty()) {
+                    observers.remove(it.key)
+                }
+                return@forEach
+            }
+        }
+        super.removeObserver(observer)
+    }
+
+    @MainThread
+    override fun setValue(value: T?) {
+        observers.forEach { it.value.forEach { wrapper -> wrapper.newValue() } }
+        super.setValue(value)
+    }
+
+    /**
+     * Used for cases where T is Void, to make calls cleaner.
+     */
+    @MainThread
+    fun call() {
+        value = null
+    }
+
+    private class ObserverWrapper<T>(private val observer: Observer<in T>) : Observer<T> {
+
+        private val pending = AtomicBoolean(false)
+
+        override fun onChanged(value: T?) {
+            if (pending.compareAndSet(true, false)) {
+                observer.onChanged(value)
+            }
+        }
+
+        fun newValue() {
+            pending.set(true)
+        }
+    }
+}
